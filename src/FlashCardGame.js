fad41a72ca7118060021b93b3c5e3ad25b48c95a
emilyalice2708 <emilyalicesesto@gmail.com>
2020-12-18 21:48:29 +0000

    Remove options from flashcards

--- src/FlashCardGame.js
+++ src/FlashCardGame.js
@@ -18,28 +18,16 @@ const QUESTIONS = [{
     id: 1,
     question: 'One',
     answer: 'Uno',
-    options: [
-        'Uno', 
-        'Dos', 
-        'Tres']
 },
 {
     id: 2,
     question: 'Two',
     answer: 'Dos',
-    options: [
-        'Uno', 
-        'Dos', 
-        'Tres']
 },
 {
     id: 3,
     question: 'Three',
     answer: 'Tres',
-    options: [
-        'Uno', 
-        'Dos', 
-        'Tres']
 }
 ]
 
